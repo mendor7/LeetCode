902b24514d47445444e71ac57c2ca5b0c73cafb2
Richard Mendoza <89940149+mendor7@users.noreply.github.com>
2024-01-02 17:04:50 -0800

    Add files via upload

---
+++ 0389-Find-the-Difference.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char, int> sTimes;
+        unordered_map<char, int> tTimes;
+        for (int i = 0; i < t.length(); i++) {
+            if (i == s.length()) {
+                tTimes[t[i]]++;
+                break;
+            }
+            sTimes[s[i]]++;
+            tTimes[t[i]]++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            if (tTimes[t[i]] != sTimes[t[i]]) {
+                return t[i];
+            }
+        }
+        return 0;
+    }
+};
